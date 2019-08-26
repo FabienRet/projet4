72df9cc714e2b13d8f0ace7278f5aa73208f0111
FabienRet <fabienretailleau@hotmail.fr>
2019-08-26 16:09:51 +0200

    add Frontcontroller

---
+++ src/controller/FrontController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\src\controller;
+
+
+use App\src\DAO\ArticleDAO;
+use App\src\DAO\CommentDAO;
+use App\src\DAO\ConnexionDAO;
+
+class FrontController
+{
+
+    private $article;
+    private $comment;
+    private $connexion;
+
+    public function __construct()
+    {
+        $this->article = new ArticleDAO();
+        $this->comment = new CommentDAO();
+        $this->connexion = new ConnexionDAO();
+    }
+
+    public function home(){
+        $result = $this->article->getArticles();
+        require '../templates/home.php';
+    }
+
+    public function getComment($idArticle){
+        $art = $this->article->getArticle($idArticle);
+        $result = $this->comment->getComment($idArticle);
+        require '../templates/comment.php';
+    }
+
+    public function addArticle($post) {
+        if(isset($post) && !empty($post)){
+            $this->article->addArticle($post['titre'], $post['article']);
+            header ('Location: ../public/index.php');
+        }
+        require '../templates/articles_add.php';
+    }
+
+    public function deleteArticle($idArticle){
+        $this->article->deleteArticle($idArticle);
+        header('Location: ../public/index.php');
+    }
+
+    public function deleteComment($idComment){
+        $this->comment->deleteComment($idComment);
+        header('Location: ../public/index.php?route=comment&article='.$_GET["ID_article"].'');
+    }
+
+    public function addComment($idArticle, $post){
+        if(isset($post) && !empty($post)){
+            $this->comment->addComment($idArticle, $post['pseudo'], $post['message']);
+            header('Location: ../public/index.php?route=comment&article='.$_GET["article"].'');
+        }
+    require '../templates/comment_add.php?article=';
+    }
+
+    public function getArticle($idArticle){
+        $result = $this->article->getArticle($idArticle);
+        require '../templates/articles_update.php';
+    }
+    public function updateArticle($idArticle, $post){
+        if(isset($post) && !empty($post)) {
+            $this->article->updateArticle($post['titre'], $post['article'], $idArticle);
+            header('Location: ../public/index.php');
+        }
+    }
+    public function comment($idComment){
+        $result = $this->comment->comment($idComment);
+        require '../templates/comment_update.php';
+    }
+    public function updateComment($idComment, $post){
+        if(isset($post) && !empty($post)) {
+            $this->comment->updateComment($idComment, $post['auteur'], $post['commentaire']);
+            header('Location: ../public/index.php?route=comment&article='.$_GET["ID_article"].'');
+        }
+    }
+    public function login($post){
+        if(isset($post) && !empty($post)){
+            if(!empty($post['pseudo']) && !empty($post['pass'])){
+                if($this->connexion->testPseudo($post['pseudo']) == 1){
+                    if($this->connexion->checkPass($post['pseudo'], $post['pass']) == 1){
+                        header('Location: ../public/index.php');
+                    }else{
+                     $alert = 'Mot de passe inconnu';
+                }
+            }else{
+                    $alert = 'Pseudo inconnu';
+                }
+            }else{
+                $alert = 'Merci de remplir tous les champs';
+            }
+        }
+            if (!empty($alert)){
+                echo "<script>alert('$alert')</script>";
+                require '../templates/login.php';
+            }
+    }
+    public function inscription($post){
+        if(isset($post) && !empty($post)){
+            if(!empty($post['pseudo']) && !empty($post['pass']) && !empty($post['checkpass']) && !empty($post['email'])){
+                if($this->connexion->testPseudo($post['pseudo']) == 0) {
+                    if($this->connexion->testPass($post['pass'], $post['checkpass']) == 0) {
+                        if($this->connexion->testEmail($post['email']) == 0){
+                            $this->connexion->connexion($post['pseudo'], $post['pass'], $post['email']);
+                            header('Location: ../public/index.php');
+                        }else{
+                            $alert = 'Cet email est déjà utilisé';
+                        }
+                        }else{
+                            $alert= "Les mots de passe ne correspondent pas !";
+                        }
+                    }else{
+                        $alert = "Ce pseudo est déjà utilisé !";
+                    }
+                }else{
+                    $alert = 'Merci de remplir tous les champs !';
+                }
+            }
+        if (!empty($alert)){
+        echo "<script>alert('$alert') </script>";
+        require'../templates/inscription.php';  
+        }
+    }
+
+}
